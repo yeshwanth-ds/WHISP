02f27a34a644346ef483d88d735e92ba470d1def
yeshwanth <yeshwanthds2002@gmail.com>
2024-10-26 21:04:11 +0530

    Backend completed

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/users.routes.js";
+import connectToMongoose from "./db/connectMongoDB.js";
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+
+// app.get("/", (req, res) => {
+//     res.send("Hello worl")
+// })
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+    connectToMongoose();
+    console.log(`Server running in port ${PORT}`);
+});
